d44eef4a16561adad25db43457f390c734b773fc
JustThinkingHard <oscar.deschamps@epitech.eu>
2026-01-29 17:06:56 +0100

    [backdoor] malware that create a reverse shell on windows

---
+++ backdoor/clean.bat
@@ -0,0 +1,17 @@
+@echo off
+echo [*] Arret du processus malveillant...
+taskkill /F /IM update.exe >nul 2>&1
+taskkill /F /IM OneDriveUpdate.exe >nul 2>&1
+
+echo [*] Suppression de la persistance (Dossier Demarrage)...
+del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\OneDriveUpdate.exe" /F /Q
+
+echo [*] Suppression du virus dans Temp...
+del "%TEMP%\doc.exe" /F /Q
+
+echo [*] Suppression du raccourci piege sur le Bureau...
+del "%USERPROFILE%\Desktop\Facture*.lnk" /F /Q
+del "%USERPROFILE%\Desktop\Facture*.pdf" /F /Q
+
+echo [+] Nettoyage termine.
+pause
